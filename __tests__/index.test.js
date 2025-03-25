16000358ebaf34272f1bfe9043bc4a2a9d148994
Shubham Khunt <shubhamkhunt08@gmail.com>
2025-03-25 15:34:52 +0530

    Test: Support new lines as delimiters

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,3 +16,8 @@ test("returns sum for multiple numbers", () => {
   expect(add("1,2,3,4,5")).toBe(15);
 });
 
+test("handles new lines between numbers", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
+
+
