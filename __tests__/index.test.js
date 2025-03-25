a331af7b8f73ff7e38a1ec9a369a080bfd9102f4
Shubham Khunt <shubhamkhunt08@gmail.com>
2025-03-25 15:29:20 +0530

    Test: Add cases for single and two numbers

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,3 +3,11 @@ const add = require("../index");
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns number itself when a single number is given", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("returns sum when two numbers are given", () => {
+  expect(add("1,5")).toBe(6);
+});
