d4cafbe82061bceef6df6cc1c827fd744c2cc86c
Shubham Khunt <shubhamkhunt08@gmail.com>
2025-03-25 15:35:31 +0530

    Feat: Allow new lines as delimiters

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function add(numbers) {
   if (!numbers) return 0;
   return numbers
-    .split(",")
+    .split(/[\n,]/)  // Allow both comma `,` and newline `\n`
     .map(Number)
     .reduce((sum, num) => sum + num, 0);
 }
